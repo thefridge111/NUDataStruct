fae51dca91981bf9ddbb51ecfc47383d6b4679c9
thefridge111 <wfperry1@gmail.com>
2014-09-05 10:36:57 -0400

    Initial Commit. Full Program.

---
+++ Assign1/LinkedList.cpp
@@ -0,0 +1,69 @@
+// LinkedList.cpp
+
+#include "LinkedList.h"
+
+// Default constructor, the list is empty to start
+
+LinkedList::LinkedList() {
+    // Add the appropriate content here  
+}
+
+// Default destructor, must delete all nodes
+
+LinkedList::~LinkedList() {
+    // Add the appropriate content here  
+
+}
+
+// Add a node containing "value" to the front
+
+void LinkedList::InsertFront(double value) {
+    // Add the appropriate content here  
+
+}
+
+// Add a node containing "value" to position "index"
+
+void LinkedList::Insert(double value, unsigned int index) {
+    // Add the appropriate content here  
+
+}
+
+
+// Return value at position "index"
+
+double LinkedList::GetNode(unsigned int index) {
+    // Add the appropriate content here  
+
+}
+
+
+// Return the index of the node containing "value"
+
+unsigned int LinkedList::Search(double value) {
+    // Add the appropriate content here  
+
+}
+
+
+// Delete the node at position "index", return the value that was there
+
+double LinkedList::DeleteNode(unsigned int index) {
+    // Add the appropriate content here  
+
+}
+
+// This function reverses the order of all nodes so the first node is now the
+// last and the last is now the first (and all between). So a list containing 
+// (4, 0, 2, 11, 29) would be (29, 11, 2, 0, 4) after Reverse() is called.
+
+void LinkedList::Reverse() {
+    // Add the appropriate content here
+}
+
+// Return the number of nodes in the list
+
+int LinkedList::GetLength() {
+    // Add the appropriate content here  
+
+}
