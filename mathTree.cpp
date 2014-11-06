277c69e5960f8b948a7a29d2e4b93ad7b6de4941
thefridge111 <wfperry1@gmail.com>
2014-11-06 09:47:48 -0500

    Project 5 finish. Debug comments removed. Descriptive comments added.

--- mathTree.cpp
+++ mathTree.cpp
@@ -15,7 +15,6 @@ string stringify(long double x) {
 
 mathTree::mathTree() {
     root = NULL;
-    // Add content here
 }
 
 mathTree::~mathTree() {
@@ -25,20 +24,22 @@ mathTree::~mathTree() {
 void mathTree::ReadExpression(string s) {
     string remaining;
     
-    root = new treeNode;
+    root = new treeNode;            //Create tree root
     
-    remaining = _readex(root, s);
-    if (remaining.length() > 0) {
-        throw 2;
+    remaining = _readex(root, s);   //Call readex to build tree
+    
+    if (remaining.length() > 0) {   //Check for remaining characters from strings
+        throw 2;                    //Only invalid strings will have characters left
     }
 }
 
 
 string mathTree::_readex(treeNode* t, string exp) {
-    int b;
-    //cout << "initial: " << exp << endl;
-    if (exp.length() > 0) {
-        while (isspace(exp[0])) {
+    int b;                          //Stores location of next space
+    
+    if (exp.length() > 0) {         //Check that there are characters in string
+                                    
+        while (isspace(exp[0])) {   //Removes leading spaces
             exp.erase(0,1);
         }
 
@@ -49,118 +50,93 @@ string mathTree::_readex(treeNode* t, string exp) {
             t->lChild = NULL;
             t->rChild = NULL;
             exp.erase(0,b);
-            //cout << "value: " << t->value << endl;
-            //cout << "exp: " << exp << endl;
             return exp;
         } else {
-            t->op = exp[0];
-            t->leafNode = false;
-            t->lChild = new treeNode;
-            t->rChild = new treeNode;
-            exp.erase(0,1);
-            //cout << "op: " << t->op << endl;
-            //cout << "exp1: " << exp << endl;
+            t->op = exp[0];                 //Sets operators from front of string
+            t->leafNode = false;            //and places into current node's op
+            t->lChild = new treeNode;       //variable. Erases front string value
+            t->rChild = new treeNode;       //Creates new children and calls readex
+            exp.erase(0,1);                 //on children; left first, right second.
             exp = _readex(t->lChild, exp);
-            //cout << "left1: " << exp << endl;
             exp = _readex(t->rChild, exp);
-            //cout << "left: " << exp << endl;
-            return exp;
+            return exp;                     //Returns remaining string.
         }
     } else {
-        cout << "No length." << endl;
-        throw 1;
+        cout << "No length." << endl;       //Notifies user of improperly formatted
+        throw 1;                            //or formed strings, throws exception
     }
 }
 
 double mathTree::ExpressionValue() {
     double result;
     
-    result = _evaluate(root);
+    result = _evaluate(root);               
     
     return result;
 }
 
 double mathTree::_evaluate(treeNode* t) {
-    char opera;
-    double result, k, l;
-    
-    if (t->lChild != NULL) {
-        k = _evaluate(t->lChild);
+    char opera;                         //Stores operator value for computation. 
+    double result, l, r;                //Stores numerical values for computation.
+                                        //l: left child value, r: right child value
+    if (t->lChild != NULL) {        
+        l = _evaluate(t->lChild);       //If not a leaf node, calls evaluate on left Child
     } 
     
-    if(t->leafNode) {
+    if(t->leafNode) {                   //If a leaf node, stores node value 
         result = t->value;
         return result;
-    } else {
+    } else {                            //If an not leaf node, stores operator
         opera = t->op;
     }
     
-    if (t->lChild != NULL) {
-        l = _evaluate(t->rChild);
+    if (t->lChild != NULL) {            //If not a leaf node, calls evaluate on right Child
+        r = _evaluate(t->rChild);
     }
-    switch(opera) {
-        case '+':
-            result = k + l;
+    
+    switch(opera) {                     //Parses operator values to act accordingly
+        case '+':                       //NOTE: Does not do modulo arithmetic because evaluating two doubles.
+            result = l + r;             //Addition
             break;
         case '-':
-            result = k - l;
+            result = l - r;             //Subtraction
             break;
-        case '*':
-            result = k * l;
+        case '*':   
+            result = l * r;             //Multiplication
             break;
         case '/':
-            result = k / l;
+            result = l / r;             //Division
             break;
         default:
-            cout << "Unrecognized operator." << endl;
-    } 
-    return result;
+            cout << "Unrecognized operator." << endl;   //If operator falls outside
+    }                                                   //recognized values.
+    return result;                      //Returns value/operator for parent computation
 }
 
 string mathTree::ReturnInfix() {
     string infix_Notation;
-    //cout << "here" << endl;
-    infix_Notation += _inorder(root);
-    
-    return infix_Notation;
+    infix_Notation += _inorder(root);   //Inorder traverses tree, formatting output
+                                        //as it goes.
+    return infix_Notation;             
 }
 
 string mathTree::_inorder(treeNode* t) {
-    string inorder_Notation;
-    //cout << "value: " << t->value << endl;
-    //cout << "op: " << t->op << endl;
-    if (t->lChild != NULL) {
-        inorder_Notation += '(';
-        inorder_Notation += _inorder(t->lChild);
+    string inorder_Notation;;
+    if (t->lChild != NULL) {            //While theres a left child continues traversing
+        inorder_Notation += '(';        //down tree to left child leaf. Adds parentheses
+        inorder_Notation += _inorder(t->lChild); //for formatting purposes.
     } 
     
     if(t->leafNode) {
-        inorder_Notation += (stringify(t->value));
+        inorder_Notation += (stringify(t->value));  //Grabs operator/value as appropriate
     } else {
         inorder_Notation += (t->op);
     }
        
-    if (t->rChild != NULL) {
-        inorder_Notation += _inorder(t->rChild);
+    if (t->rChild != NULL) {            //Traverses to end of right children. Closes parentheses for 
+        inorder_Notation += _inorder(t->rChild);    //formatting purposes.
         inorder_Notation += ')';
     }
     
     return inorder_Notation;
-    
-    /*string inorder_Notation;
-    
-    if (t->lChild != NULL) {
-        inorder_Notation += _inorder(t->lChild);
-    } 
-    
-    if(t->leafNode) {
-        inorder_Notation += (t->value + ',');
-    } else {
-        inorder_Notation += (t->op + ',');
-    }
-    
-    if (t->rChild != NULL) {
-        inorder_Notation += _inorder(t->rChild);
-    }
-    */
 }
