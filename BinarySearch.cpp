ddd8391a139f98b88c9c45c7a0d8278026d9c73f
thefridge111 <wfperry1@gmail.com>
2014-11-13 09:47:22 -0500

    Project 6 finished. Commented. Read through for error checks. Could be optimized.

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -16,45 +16,45 @@ int search(int A[], unsigned int L, int target, bool sorted) {
     unsigned int first, last, middle, tempMid;
     
     if (L > 0) {;
-        middle = (L / 2) + 1;
-        first = 0;
-        last = L - 1;
+        middle = (L / 2) + 1;       //Defines an initial 'middle' of the array
+        first = 0;                  //Holds the first value of array/array subset 
+        last = L - 1;               //Holds the last value of array/array subset
         
-        if (!sorted) {
-            for (unsigned int i = 0; i <= L; i++) {
-                if (A[i] == target) {
-                    return i;
+        if (!sorted) {              //Checks if array is sorted
+            for (unsigned int i = 0; i <= L; i++) { //Begin serial search
+                if (A[i] == target) {               //Check if array value is target
+                    return i;                       //Returns index value if found
                 }
             }
-            return -1;
+            return -1;              //Return -1 if not found              
         } else {
-            if (target == A[middle]) {
-                return A[middle];
+            if (target == A[middle]) {      //Checks if middle is target value
+                return middle;              //Returns middle index value
             }
             else {
-                tempMid = middle;
-                for (unsigned int j = 0; j < middle; j++) {
-                    if (tempMid < 0 || tempMid >= L) {
-                        throw "Invalid index value. Index exceed size.";
-                    } else if(target < A[tempMid]) {
-                        last = tempMid;
-                        if ((last - first) == 1) {
+                tempMid = middle;           //Sets a middle value that will change
+                for (unsigned int j = 0; j < middle; j++) { 
+                    if (tempMid < 0 || tempMid >= L) {  //Checks if walked off end of array
+                        throw "Invalid index value. Index exceeds size.";
+                    } else if(target < A[tempMid]) {    //Checks if target is less than middle
+                        last = tempMid;                 //Moves end of array subset to middle value
+                        if ((last - first) == 1) {      //If difference is 1 then decrement middle value
                             tempMid--;
                         } else {
-                        tempMid = (first + ((last - first) / 2));
+                        tempMid = (first + ((last - first) / 2)); //Moves middle value halfway between first and last values
                         }
-                    } else if ( target > A[tempMid]) {
-                        first = tempMid;
-                        if ((last - first) == 1) {
-                            tempMid++;
+                    } else if ( target > A[tempMid]) {  //Checks if target is greater than middle
+                        first = tempMid;                //Moves beginning of array subset to middle value
+                        if ((last - first) == 1) {      //If difference is 1 then increment middle value
+                            tempMid++;                  
                         } else {
-                        tempMid = (first + ((last - first) / 2));
+                        tempMid = (first + ((last - first) / 2)); //Moves middle value halfway between first and last values
                         }
-                    } else if (target == A[tempMid]) {
+                    } else if (target == A[tempMid]) {  //Check if target is at middle index value 
                         return tempMid;
                     } 
                 }
-                return -1;
+                return -1;          //If value is not in array
             } 
         }
     }
